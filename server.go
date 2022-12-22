e87458043a61e0e75c11b17604b8679fa83c4db4
Somphong Phuangmalai <somphong.ph@gmail.com>
2022-12-22 09:01:52 +0700

    init db

--- server.go
+++ server.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/somphongph/assessment/internal/expense"
 )
 
 func main() {
+	expense.InitDB()
+
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
 }
