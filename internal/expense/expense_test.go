b3a854ea65e0e7f24199b3c44e90e7f4a44ad2ee
Somphong Phuangmalai <somphong.ph@gmail.com>
2022-12-24 09:45:40 +0700

    Add test

---
+++ internal/expense/expense_test.go
@@ -0,0 +1,72 @@
+package expense
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAllUser(t *testing.T) {
+	seedUser(t)
+
+	var us []Expense
+	res := request(http.MethodGet, uri("expenses"), nil)
+	err := res.Decode(&us)
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, http.StatusOK, res.StatusCode)
+	assert.Greater(t, len(us), 0)
+}
+
+func seedUser(t *testing.T) Expense {
+	var c Expense
+	body := bytes.NewBufferString(`{
+		"title": "strawberry smoothie",
+		"amount": 79,
+		"note": "night market promotion discount 10 bath",
+		"tags": ["food", "beverage"]
+	}`)
+	err := request(http.MethodPost, uri("expenses"), body).Decode(&c)
+	if err != nil {
+		t.Fatal("can't create uomer:", err)
+	}
+	return c
+}
+
+func uri(paths ...string) string {
+	host := "http://localhost:2565"
+	if paths == nil {
+		return host
+	}
+
+	url := append([]string{host}, paths...)
+	return strings.Join(url, "/")
+}
+
+type Response struct {
+	*http.Response
+	err error
+}
+
+func (r *Response) Decode(v interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func request(method, url string, body io.Reader) *Response {
+	req, _ := http.NewRequest(method, url, body)
+	req.Header.Add("Authorization", os.Getenv("AUTH_TOKEN"))
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	res, err := client.Do(req)
+	return &Response{res, err}
+}
