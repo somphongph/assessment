dcda0e9a7441fb7783dfb505c539b188d23927f4
Somphong Phuangmalai <somphong.ph@gmail.com>
2022-12-24 15:23:31 +0700

    set array to result

--- internal/expense/get.go
+++ internal/expense/get.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 	"github.com/somphongph/assessment/internal/model"
 )
 
@@ -12,18 +13,18 @@ func (h *Handler) GetExpenseHandler(c echo.Context) error {
 	id := c.Param("id")
 	stmt, err := h.db.Prepare("SELECT id, title, amount, note, tags FROM expenses WHERE id = $1")
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, model.Err{Message: "can't prepare query user statment:" + err.Error()})
+		return c.JSON(http.StatusInternalServerError, model.Err{Message: "can't prepare query expense statment:" + err.Error()})
 	}
 
 	row := stmt.QueryRow(id)
 	e := Expense{}
-	err = row.Scan(&e.Id, &e.Title, &e.Amount, &e.Note, &e.Tags)
+	err = row.Scan(&e.Id, &e.Title, &e.Amount, &e.Note, (*pq.StringArray)(&e.Tags))
 	switch err {
 	case sql.ErrNoRows:
-		return c.JSON(http.StatusNotFound, model.Err{Message: "user not found"})
+		return c.JSON(http.StatusNotFound, model.Err{Message: "expense not found"})
 	case nil:
 		return c.JSON(http.StatusOK, e)
 	default:
-		return c.JSON(http.StatusInternalServerError, model.Err{Message: "can't scan user:" + err.Error()})
+		return c.JSON(http.StatusInternalServerError, model.Err{Message: "can't scan expense:" + err.Error()})
 	}
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -28,7 +28,7 @@ func NewRouter() *echo.Echo {
 	expense := e.Group("/expenses")
 	{
 		expense.POST("", expenseHandler.CreateExpenseHandler)
-		expense.GET(":id", expenseHandler.GetExpenseHandler)
+		expense.GET("/:id", expenseHandler.GetExpenseHandler)
 	}
 
 	log.Fatal(e.Start(os.Getenv("PORT")))
