4c6f3b6a7fe946b97b07ed96528b2f0901f246fe
Somphong Phuangmalai <somphong.ph@gmail.com>
2022-12-24 14:55:37 +0700

    fix test

--- internal/expense/expense_test.go
+++ internal/expense/expense_test.go
@@ -3,39 +3,39 @@ package expense
 import (
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"strings"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 )
 
 var (
-	expenseJSON = `{"title":"buy a new phone","amount":39000,"note":"buy a new phone"}`
+	expenseJSON = `{"title":"buy a new phone","amount":39000,"note":"buy a new phone","tags":["food", "beverage"]}`
 )
 
 func TestCreateExpense(t *testing.T) {
-	db, _, err := sqlmock.New()
+	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer db.Close()
 
-	// mock.ExpectBegin()
-	// // // mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
-	// mock.ExpectExec(regexp.QuoteMeta(`INSERT INTO "expenses" ("title", "amount", "note")
-	// 	VALUES ($1, $2, $3) RETURNING id`)).
-	// 	WithArgs("buy a new phone", 39000, "buy a new phone").
-	// 	WillReturnResult(sqlmock.NewResult(1, 1))
-	// // mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
-	// // mock.ExpectExec("INSERT INTO product_viewers").WithArgs(2, 3).WillReturnResult(sqlmock.NewResult(1, 1))
-	// mock.ExpectCommit()
+	query := `INSERT INTO "expenses" ("title", "amount", "note", "tags") VALUES ($1,$2,$3,$4) RETURNING "expenses"."id"`
+	mock.MatchExpectationsInOrder(false)
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(query)).
+		WithArgs("buy a new phone", 39000, "buy a new phone", pq.Array([]string{"food", "beverage"})).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
 
 	// Setup
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(expenseJSON))
+	req := httptest.NewRequest(http.MethodPost, "/expenses", strings.NewReader(expenseJSON))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
