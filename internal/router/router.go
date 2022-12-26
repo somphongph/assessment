d7db13e2973588c3cf064538439de0c856652e89
Somphong Phuangmalai <somphong.ph@gmail.com>
2022-12-26 09:35:18 +0700

    Graceful shutdown

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,10 +1,14 @@
 package router
 
 import (
+	"context"
 	"crypto/subtle"
 	"database/sql"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -43,7 +47,20 @@ func NewRouter() *echo.Echo {
 		expense.PUT("/:id", expenseHandler.UpdateExpenseHandler)
 	}
 
-	log.Fatal(e.Start(os.Getenv("PORT")))
+	go func() {
+		if err := e.Start(os.Getenv("PORT")); err != nil && err != http.ErrServerClosed { // Start server
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 
 	return e
 }
